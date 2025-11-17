98fd91e7b99fc069789ccc3ae76286ce9f361fcd
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-11-17 21:24:07 +0500

    update

--- train_zero.py
+++ train_zero.py
@@ -45,6 +45,7 @@ def main():
     parser.add_argument('--obj', type=str, default='Retina_RESC')
     parser.add_argument('--data_path', type=str, default='./data/')
     parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--save_path', type=str, default='./ckpt/few-shot/')
     parser.add_argument('--img_size', type=int, default=240)
     parser.add_argument("--epoch", type=int, default=50, help="epochs")
     parser.add_argument("--learning_rate", type=float, default=0.0001, help="learning rate")
