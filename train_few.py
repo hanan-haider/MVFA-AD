88b0c985575663fb390c676d94d15d40024c581c
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-11-24 14:39:06 +0500

    update

--- train_few.py
+++ train_few.py
@@ -11,7 +11,10 @@ from scipy.ndimage import gaussian_filter
 from dataset.medical_few import MedDataset
 from CLIP.clip import create_model
 from CLIP.tokenizer import tokenize
-from CLIP.adapter import CLIP_Inplanted
+
+from CLIP.improvedadapter import CLIP_Inplanted
+
+#from CLIP.adapter import CLIP_Inplanted
 from PIL import Image
 from sklearn.metrics import roc_auc_score, precision_recall_curve, pairwise
 from loss import FocalLoss, BinaryDiceLoss
@@ -64,7 +67,7 @@ def main():
     clip_model = create_model(model_name=args.model_name, img_size=args.img_size, device=device, pretrained=args.pretrain, require_pretrained=True)
     clip_model.eval()
 
-    model = CLIP_Inplanted(clip_model=clip_model, features=args.features_list).to(device)
+    model = EnhancedCLIPAdapter(clip_model=clip_model, features=args.features_list, r=16,   alpha=32)).to(device)
     model.eval()
 
     for name, param in model.named_parameters():
